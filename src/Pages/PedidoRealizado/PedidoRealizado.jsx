19c1fbd4a35ecf658fe26e5c8b01520f9f6513ca
ArielGarav <arielgaraventa5@gmail.com>
2023-06-06 05:05:31 -0300

    reparando error pedido realizado

--- src/Pages/PedidoRealizado/PedidoRealizado.jsx
+++ src/Pages/PedidoRealizado/PedidoRealizado.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-import Button from "../../components/UI/Button/Button";
+import Button from "../../Components/UI/Button/Button";
 
 import {
   ContainerInfoStyled,
